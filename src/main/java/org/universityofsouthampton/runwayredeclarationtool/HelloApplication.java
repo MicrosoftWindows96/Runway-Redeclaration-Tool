a747772b6378cc2b14330fabb3fa261d3939713b
Zagrosi <sz10g21@soton.ac.uk>
2024-02-27 21:43:18 +0000

    Add new IDE configuration files and update HelloApplication
    New IntelliJ IDEA configuration files have been added including vcs.xml, misc.xml, .gitignore, and encodings.xml. These configuration files will improve project setup for new contributors. Also, the HelloApplication.java file has been updated to include a menu display feature for the application's user interface.

--- src/main/java/org/universityofsouthampton/runwayredeclarationtool/HelloApplication.java
+++ src/main/java/org/universityofsouthampton/runwayredeclarationtool/HelloApplication.java
@@ -1,23 +1,49 @@
 package org.universityofsouthampton.runwayredeclarationtool;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+    public void start(Stage stage) {
+        // Create a Menu
+        Menu menu = new Menu("Menu");
+
+        // Create menuitems
+        MenuItem item1 = new MenuItem("Item 1");
+        MenuItem item2 = new MenuItem("Item 2");
+
+        // Add menu items to the menu
+        menu.getItems().add(item1);
+        menu.getItems().add(item2);
+
+        // Create a menubar
+        MenuBar menubar = new MenuBar();
+
+        // Add menu to the menubar
+        menubar.getMenus().add(menu);
+
+        // Create a border pane
+        BorderPane bPane = new BorderPane();
+
+        // Set menubar to the top of border pane
+        bPane.setTop(menubar);
+
+        // Create a scene
+        Scene scene = new Scene(bPane, 960, 600);
+
+        // Set the scene
         stage.setScene(scene);
+
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }

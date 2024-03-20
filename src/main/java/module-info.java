218916cfcc38550db1482f88d690c53d9aa7b03f
Zagrosi <sz10g21@soton.ac.uk>
2024-03-20 08:47:47 +0000

    Add and reorganize module dependencies
    Updated module-info.java to include new dependencies such as javafx.base, javafx.graphics, org.kordamp.ikonli.core, org.kordamp.ikonli.javafx, and org.kordamp.ikonli.fontawesome. In addition, the order of the required modules has been adjusted for clarity and ease of reading, ensuring a more maintainable and understandable code base.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,16 +3,21 @@ module org.universityofsouthampton.runwayredeclarationtool {
     requires javafx.fxml;
     requires javafx.web;
 
+    requires javafx.base;
+    requires javafx.graphics;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
     requires org.kordamp.ikonli.fontawesome5;
     requires java.desktop;
     requires org.kordamp.ikonli.materialdesign;
+    requires org.kordamp.ikonli.fontawesome;
     requires java.xml.bind;
     requires annotations;
     requires javafx.media;
@@ -23,6 +28,6 @@ module org.universityofsouthampton.runwayredeclarationtool {
     opens org.universityofsouthampton.runwayredeclarationtool.airport to javafx.fxml;
     exports org.universityofsouthampton.runwayredeclarationtool.utility;
     opens org.universityofsouthampton.runwayredeclarationtool.utility to javafx.fxml;
-  exports org.universityofsouthampton.runwayredeclarationtool.UI;
-  opens org.universityofsouthampton.runwayredeclarationtool.UI to javafx.fxml;
+    exports org.universityofsouthampton.runwayredeclarationtool.UI;
+    opens org.universityofsouthampton.runwayredeclarationtool.UI to javafx.fxml;
 }

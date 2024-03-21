441de8f46752e08fbc84abb296f701c46618aa58
Anthony Geron <ag7g22@soton.ac.uk>
2024-03-21 15:09:25 +0000

    Added partition and boundary test in ObstacleTest

--- src/test/java/ObstacleTest.java
+++ src/test/java/ObstacleTest.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.universityofsouthampton.runwayredeclarationtool.airport.Obstacle;
+import org.universityofsouthampton.runwayredeclarationtool.airport.Runway;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ObstacleTest {
     Obstacle obstacle;
@@ -31,6 +35,76 @@ class ObstacleTest {
         assertEquals(21, obstacle.getHeight());
         assertEquals(100, obstacle.getDistanceFromThreshold());
         assertEquals(123, obstacle.getDistanceFromCentreline());
+    }
+
+    @Test
+    void testValidObstacle(){ // TESTING RANGES PARTITIONS
+        Runway runway = new Runway("09",0,0,3660,307);
+        assertTrue(obstacle.validObstacle(runway));
+
+        obstacle.setDistanceFromCentreline(50);
+        assertTrue(obstacle.validObstacle(runway)); // valid distance from centre line
+
+        obstacle.setDistanceFromCentreline(150);
+        assertFalse(obstacle.validObstacle(runway)); // invalid distance from centre line
+
+        obstacle.setDistanceFromCentreline(50);
+        obstacle.setHeight(20);
+        assertTrue(obstacle.validObstacle(runway));  // valid height
+
+        obstacle.setHeight(-10);
+        assertFalse(obstacle.validObstacle(runway)); // invalid height (HEIGHT NEGATIVE)
+
+        obstacle.setHeight(40);
+        assertFalse(obstacle.validObstacle(runway)); // invalid height (HEIGHT TOO LARGE)
+
+        obstacle.setHeight(20);
+        obstacle.setDistanceFromThreshold(200);
+        assertTrue(obstacle.validObstacle(runway)); // valid distance from threshold
+
+        obstacle.setDistanceFromThreshold(4000);
+        assertFalse(obstacle.validObstacle(runway)); // invalid distance from threshold (DISTANCE BEYOND TORA)
+
+        obstacle.setDistanceFromThreshold(-400);
+        assertFalse(obstacle.validObstacle(runway)); // invalid distance from threshold (DISTANCE BEYOND DISPLACED THRESHOLD)
+    }
+
+    @Test
+    void testObstacleBoundaries() {
+        Runway runway = new Runway("09",0,0,3660,307);
+        assertTrue(obstacle.validObstacle(runway));
+
+        obstacle.setDistanceFromCentreline(99);
+        assertTrue(obstacle.validObstacle(runway)); // Distance from centre < 100
+
+        obstacle.setDistanceFromCentreline(100);
+        assertFalse(obstacle.validObstacle(runway)); // Distance from centre > 100
+
+        obstacle.setDistanceFromCentreline(50);
+        obstacle.setHeight(34);
+        assertTrue(obstacle.validObstacle(runway));  // Height < 35
+
+        obstacle.setHeight(35);
+        assertFalse(obstacle.validObstacle(runway)); // Height > 35
+
+        obstacle.setHeight(10);
+        assertTrue(obstacle.validObstacle(runway));  // Height < 10
+
+        obstacle.setHeight(9);
+        assertFalse(obstacle.validObstacle(runway)); // Height > 10
+
+        obstacle.setHeight(20);
+        obstacle.setDistanceFromThreshold(3659);
+        assertTrue(obstacle.validObstacle(runway)); // Distance from threshold < TORA
+
+        obstacle.setDistanceFromThreshold(3660);
+        assertFalse(obstacle.validObstacle(runway)); // Distance from threshold > TORA
+
+        obstacle.setDistanceFromThreshold(-306);
+        assertTrue(obstacle.validObstacle(runway)); // Distance from threshold < - displaced threshold
+
+        obstacle.setDistanceFromThreshold(-307);
+        assertFalse(obstacle.validObstacle(runway)); // Distance from threshold > - displaced threshold
     }
 
     @Test

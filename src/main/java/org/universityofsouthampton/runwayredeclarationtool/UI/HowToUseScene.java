776708e662da68309116c02f787d743891061273
Zagrosi <sz10g21@soton.ac.uk>
2024-05-01 20:37:16 +0100

    test

--- src/main/java/org/universityofsouthampton/runwayredeclarationtool/UI/HowToUseScene.java
+++ src/main/java/org/universityofsouthampton/runwayredeclarationtool/UI/HowToUseScene.java
@@ -1,5 +1,6 @@
 package org.universityofsouthampton.runwayredeclarationtool.UI;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -28,6 +29,11 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import java.awt.image.BufferedImage;
+
 public class HowToUseScene extends BaseScene {
 
     public HowToUseScene(MainApplication app) {
@@ -45,8 +51,17 @@ public class HowToUseScene extends BaseScene {
         buttons.setAlignment(Pos.CENTER);
         buttons.getChildren().addAll(addButtons());
 
+        // Load and display PDF in a scrollable viewer
+        ScrollPane pdfScrollPane = createPDFViewerScrollPane("src/main/resources/HelpGuide/HowToUse.pdf");
+
         this.getChildren().addAll(title,buttons);
 
+        BorderPane rootPane = new BorderPane();
+        rootPane.setTop(title);
+        rootPane.setCenter(pdfScrollPane);
+        rootPane.setBottom(buttons);
+
+        this.getChildren().add(rootPane);
     }
 
     @Override
@@ -60,5 +75,39 @@ public class HowToUseScene extends BaseScene {
 
 
         return new ArrayList<>(Arrays.asList(backButton));
+    }
+
+    private ScrollPane createPDFViewerScrollPane(String filePath) {
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setFitToWidth(false);
+
+        try {
+            PDDocument document = PDDocument.load(new File(filePath));
+            int numPages = document.getNumberOfPages();
+
+            VBox pagesBox = new VBox(); // Container to hold all pages vertically
+
+            for (int i = 0; i < numPages; i++) {
+                PDFRenderer renderer = new PDFRenderer(document);
+
+                // Render the current page of the PDF
+                BufferedImage image = renderer.renderImageWithDPI(i, 87); // 72 DPI
+
+                // Convert BufferedImage to JavaFX Image
+                javafx.scene.image.Image fxImage = SwingFXUtils.toFXImage(image, null);
+
+                // Display the image in an ImageView
+                javafx.scene.image.ImageView imageView = new javafx.scene.image.ImageView(fxImage);
+
+                pagesBox.getChildren().add(imageView); // Add the image to the VBox
+            }
+
+            scrollPane.setContent(pagesBox); // Set the VBox as the content of the ScrollPane
+            document.close(); // Close the PDF document when done
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return scrollPane;
     }
 }
